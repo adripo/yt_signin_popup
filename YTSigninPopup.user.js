b7f89f0aa563d996dea73abf03cd87899d69aba8
adripo <26493496+adripo@users.noreply.github.com>
2021-04-21 15:13:12 +0200

    fixed icon metadata

--- YTSigninPopup.user.js
+++ YTSigninPopup.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Youtube Disable Sign-in Popup
 // @namespace    http://youtube.com/
-// @version      1.0
+// @version      1.0.1
 // @author       adripo
 // @description  Youtube Disable Sign-in Popup
 // @homepage     https://github.com/adripo/yt_signin_popup
-// @icon         https://keepa.com/favicon.ico
+// @icon         https://www.youtube.com/favicon.ico
 // @updateURL    https://raw.githubusercontent.com/adripo/yt_signin_popup/main/YTSigninPopup.meta.js
 // @downloadURL  https://raw.githubusercontent.com/adripo/yt_signin_popup/main/YTSigninPopup.user.js
 // @supportURL   https://github.com/adripo/yt_signin_popup/wiki
