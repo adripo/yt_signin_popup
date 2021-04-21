45cfb93c7e03e5d6f9a123730de3f0538250d780
adripo <26493496+adripo@users.noreply.github.com>
2021-04-21 15:08:39 +0200

    Added meta.js

---
+++ YTSigninPopup.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Youtube Disable Sign-in Popup
+// @namespace    http://youtube.com/
+// @version      1.0
+// @description  Youtube Disable Sign-in Popup
+// @author       You
+// @match        https://www.youtube.com/*
+// @match        https://youtube.com/*
+// @grant        none
+// ==/UserScript==
